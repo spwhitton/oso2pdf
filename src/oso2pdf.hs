a0a9b84a75f34c83c1975cd0ddcfd89b0fa08542
Sean Whitton <spwhitton@spwhitton.name>
2015-09-20 18:20:42 -0700

    licence notice

--- src/oso2pdf.hs
+++ src/oso2pdf.hs
@@ -1,3 +1,26 @@
+{-
+
+    oso2pdf --- Better conversion of Oxford Scholarship Online material to PDF
+
+    Copyright (C) 2015  Sean Whitton
+
+    This file is part of oso2pdf.
+
+    oso2pdf is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    oso2pdf is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with oso2pdf.  If not, see <http://www.gnu.org/licenses/>.
+
+-}
+
 {-# LANGUAGE TemplateHaskell #-}
 
 import           Control.Lens        hiding (argument)
